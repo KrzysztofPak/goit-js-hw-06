a67d6c11589a7b41babc96cc200b382927683765
KrzysztofPak <117757357+KrzysztofPak@users.noreply.github.com>
2023-03-19 13:07:37 +0100

    amendments in task1, task2, task6

--- js/task-02.js
+++ js/task-02.js
@@ -7,20 +7,14 @@ const ingredients = [
   'Condiments',
 ];
 
-// znajdź listę ul#ingredients
 const ingredientsList = document.querySelector('#ingredients');
 
-// utwórz element <li> dla każdego elementu w tablicy ingredients i dodaj go do listy
 ingredients.forEach(ingredient => {
-  // utwórz nowy element <li>
   const li = document.createElement('li');
   
-  // dodaj zawartość tekstową do elementu <li>
   li.textContent = ingredient;
   
-  // dodaj klasę 'item' do elementu <li>
   li.classList.add('item');
   
-  // dodaj element <li> do listy ul#ingredients
   ingredientsList.appendChild(li);
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,7 @@
-const refInput = document.querySelector('#validation-input')
-const CssClass = {
-  VALID: 'valid',
-  INVALID: 'invalid',
-}
+const inputEl = document.getElementById('validation-input');
 
-refInput.addEventListener('blur', handleBlurInput)
-
-function handleBlurInput({target: {value}}) {
-  if (value.length === Number(refInput.dataset.length)) {
-    refInput.classList.add(CssClass.VALID);
-    if (refInput.classList.contains(CssClass.INVALID)) refInput.classList.remove(CssClass.INVALID);
-    return;
-  }
-  refInput.classList.add(CssClass.INVALID);
-  if (refInput.classList.contains(CssClass.VALID)) refInput.classList.remove(CssClass.VALID);
-}
+inputEl.addEventListener('blur', () => {
+  inputEl.value.length === +inputEl.dataset.length
+    ? (inputEl.className = 'valid')
+    : (inputEl.className = 'invalid');
+});
